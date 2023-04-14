83fbfa0a18cb0b0ad80f3236cf2cac4625d1af3d
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-04-14 17:01:23 +0300

    last commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-import { ExampleComponent } from 'advertui'
+import { Button } from 'advertui'
 import 'advertui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Button type={'primary'} text='primary button' />
+      <Button type={'default'} text='default button' />
+      <Button type={'dashed'} text='dashed button' />
+      <Button type={'text'} text='text button' />
+      <Button type={'link'} text='link button' />
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 import React from 'react'
-import styles from './styles.module.css'
+import './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+var styles = {
+  primary: '_3ljpl',
+  dashed: '_27xYC',
+  text: '_5bXm4',
+  link: '_71S8l',
+  default: '_3UK6o'
+}
+
+export const Button = (props) => {
+  if (props.type === 'primary') {
+    return (
+      <button className={styles.primary} {...props}>
+        {props.text}
+      </button>
+    )
+  } else if (props.type === 'default') {
+    return (
+      <button className={styles.default} {...props}>
+        {props.text}
+      </button>
+    )
+  } else if (props.type === 'dashed') {
+    return (
+      <button className={styles.dashed} {...props}>
+        {props.text}
+      </button>
+    )
+  } else if (props.type === 'text') {
+    return (
+      <button className={styles.text} {...props}>
+        {props.text}
+      </button>
+    )
+  } else if (props.type === 'link') {
+    return (
+      <button className={styles.link} {...props}>
+        {props.text}
+      </button>
+    )
+  }
 }
